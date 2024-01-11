4a55ffa76e1dc4c853d1f082c4ad69a742754977
Erin Flaherty <eflaherty007@gmail.com>
2024-01-10 19:22:23 -0700

    More string fun
    From Python crash course textbook. I know, a real live textbook, pretty awesome!

---
+++ python/python_bible/name_cases.py
@@ -0,0 +1,7 @@
+# use a variable to represent a person's name,
+# and then print that person's name in lowercase, uppercase, and title case.
+#
+name = "Ada Lovelace"
+print(name.lower())
+print(name.upper())
+print(name.title())
