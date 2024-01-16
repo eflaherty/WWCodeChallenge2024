d07c985822cd40c3ac709ed9676de063a8e3d7e3
Erin Flaherty <eflaherty007@gmail.com>
2024-01-16 15:59:20 -0700

    add if and compare logic exercises

---
+++ python/python_bible/logic/compare.py
@@ -0,0 +1,11 @@
+num1 = 10
+num2 = 27
+
+if num1 > num2:
+    print("num1 is bigger than num2")
+elif num1 == num2:
+    print("num1 is equal to num2")
+elif num2 > num1:
+    print("num2 is bigger than num1")
+else:
+    print("I have no idea :P")
