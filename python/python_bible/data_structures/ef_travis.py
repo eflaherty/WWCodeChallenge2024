c54dd374cd3db9b5069e4180e679fa41f8eadcc6
Erin Flaherty <eflaherty007@gmail.com>
2024-01-23 14:25:28 -0700

    python datastructures day 1
    python bible section 7

---
+++ python/python_bible/data_structures/ef_travis.py
@@ -0,0 +1,25 @@
+known_users = ["Lisa", "Carrie", "Jesse", "Kim", "Kendra", "Anne", "Jill", "Heather", "Erin"]
+
+print(len(known_users))
+
+while True:
+    print("Hi! My name is Travis")
+    name = input("What is your name?: ").strip().capitalize()
+
+    if name in known_users:
+        print(f"Hello, {name}")
+        remove = input("Would you like to be removed from the system (y/n)?: ").lower()
+
+        if remove == "y":
+            known_users.remove(name)
+        elif remove == "n":
+            print("No problem, I didn't want you to leave anyway!")
+
+    else:
+        print(f"Hmm, I don't think I've met you yet {name}")
+        add_me = input("Would you like to be added to the system (y/n)?: ").strip().lower()
+        if add_me == "y":
+            known_users.append(name)
+        elif add_me == "n":
+            print("No worries, see you around!")
+            
