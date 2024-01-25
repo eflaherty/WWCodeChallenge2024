d38c59d38aef8fa0c72615829d846b1be5c115b9
Erin Flaherty <eflaherty007@gmail.com>
2024-01-25 15:24:45 -0700

    Updated students.py
    Reviewed dictionaries and updated students script.

--- python/python_bible/data_structures/students.py
+++ python/python_bible/data_structures/students.py
@@ -1,5 +1,5 @@
 students = {
-            "Alice": {"id": "ID0001", "age":26, "grade":"A"},
+            "Alice": {"id": "ID0001", "age":26, "grade":"A"}, # each key in this dict has it's own dictionary
             "Bob": {"id": "ID0002", "age":27, "grade":"B"},
             "Claire": {"id": "ID0003", "age":17, "grade":"C"},
             "Dan": {"id": "ID0004", "age":21, "grade":"D"},
@@ -9,3 +9,7 @@ students = {
 print(students["Dan"]["age"])
 
 print(students["Emma"]["id"], students["Emma"]["grade"])
+
+print(students["Claire"])
+
+print(students["Bob"]["grade"])
